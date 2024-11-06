40804d95bcb7b3626abdce048873ee3646afe41c
tranquocdong1 <tranquocdong10.2@gmail.com>
2024-11-06 10:42:33 +0700

    update quantity fix error

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
+  quantity: {type: Number, required: true},
   imageUrl: { type: String, required: true },
 });
 
